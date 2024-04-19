9853e5d0b64d2c3713d6a22792fd46cf755a4c1e
nandini nanabala <nandu.snowsql@gmail.com>
2024-04-19 14:39:02 +0000

    commit and sync

--- models/Prb/customertotal.sql
+++ models/Prb/customertotal.sql
@@ -4,20 +4,26 @@
     schema="TEAMA"
   )
 }}
-with customertotal as(
-SELECT C.CUSTOMERID
-,C.FIRSTNAME,
-C.LASTNAME,
-C.COMPANY,
-C.CITY,
-C.COUNTRY,
-C.EMAIL,
-S.ORDERID,
-S.CREATEDATE,
-S.STOREID,
-S.STATUS
-FROM  {{source("my_project",'customer')}} C 
-inner join JOIN {{source("my_project",'orders')}} S 
-ON C.CUSTOMERID=S.CUSTOMERID
+WITH customertotal AS (
+    SELECT 
+        C.CUSTOMERID,
+        C.FIRSTNAME,
+        C.LASTNAME,
+        C.COMPANY,
+        C.CITY,
+        C.COUNTRY,
+        C.EMAIL,
+        S.ORDERID,
+        S.CREATEDATE,
+        S.STOREID,
+        S.STATUS
+    FROM  
+        {{ source("my_project", 'customer') }} C 
+    INNER JOIN 
+        {{ source("my_project", 'orders') }} S 
+    ON 
+        C.CUSTOMERID = S.CUSTOMERID
 )
-select CUSTOMERID,COUNT(*) FROM customertotal GROUP BY ALL;
+
+SELECT 
+ count(*) as customercount,CUSTOMERID from customertotal group by all
